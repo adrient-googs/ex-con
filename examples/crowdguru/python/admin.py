57eabe1e6267c5a1d03776f236f6e0bb42d68e08
Adrien Treuille <adrient@google.com>
2012-12-10 22:49:38 -0800

    Tested the uniqueness property works.

--- examples/crowdguru/python/admin.py
+++ examples/crowdguru/python/admin.py
@@ -20,7 +20,10 @@ class AdminHandler(webapp.RequestHandler):
     self.response.out.write(template.render(path, template_values))
 
   def get(self):
-    self.Render('admin.html', ())
+    template_values = {
+      'adminFuncs' : ADMIN_FUNCS,
+    }
+    self.Render('admin.html', template_values)
 
 @handlers.text_handler
 def addDefaultCategories(out):
@@ -37,6 +40,33 @@ def addDefaultCategories(out):
      category = Category(name=category_name)
      category.put()
      out.write('% 20s - created!\n' % category_name)
+
+@handlers.text_handler     
+def tryAddingTwoOfTheSameCategory(out):
+  """Test out adding two of the same category to make sure that it's unique."""
+  category_name = 'cooking'
+
+  def printAllWithName():
+    """Prints all categoires with name category_name."""
+    out.write('Getting all categories with name "%s."\n' % category_name)
+    categories = Category.all().filter('name =', category_name).fetch(limit=100)
+    # categories = Category.all().fetch(limit=100)
+    for ii, category in enumerate(categories):
+      if ii % 222 == 1:
+        continue
+      out.write(' %.2i - "%s"\n' % (ii, category.name))
+    out.write('Found %i categories.\n' % len(categories))    
+  
+  printAllWithName()  
+  for ii in xrange(10):
+    new_category = Category(name=category_name)
+    new_category.put()
+    out.write('Put new category name=%s key=%s.\n' % (new_category.name, new_category.key()))
+  printAllWithName()
+  if len(Category.all().filter('name =', category_name).fetch(limit=100)) == 1:
+    out.write('TEST PASSED\n')
+  else:
+    out.write('TEST FAILED\n')
      
 @handlers.text_handler
 def addDefaultUsers(out):
@@ -44,14 +74,16 @@ def addDefaultUsers(out):
   pass
 
 # This is is a list of functions which the administrator can call.
-ADMIN_FUNCTIONS = [
-  ('addDefaultCategories', 'Add some default categories'),
-  ('addDefaultUsers',      'Adds Karishma, Charles, and Adrien as users'),
-]
+ADMIN_FUNCS = (
+  ('addDefaultCategories',          'Add some default categories'),
+  ('tryAddingTwoOfTheSameCategory', 'Run a test to verify that catgories are unique'),
+  ('addDefaultUsers',               'Adds Karishma, Charles, and Adrien as users'),
+)
         
 def getHandlers():
   """Returns the handlers defined in this module."""
   handlers = [('/admin', AdminHandler)]
-  for name, description in ADMIN_FUNCTIONS:
+  for name, description in ADMIN_FUNCS:
     handlers.append(('/admin/%s' % name, globals()[name]))
+  return handlers
   
